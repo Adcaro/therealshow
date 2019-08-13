73a691782e3b88b64b2589446b5606d2cbf0e1b9
Adri <adrica3018@gmail.com>
2019-08-13 13:24:21 +0200

    Try to make online request found

--- bot.py
+++ bot.py
@@ -14,15 +14,17 @@ TOKEN = BOT_KEY
 if mode == "dev":
     def run(updater):
         updater.start_polling()
+        updater.idle()
 elif mode == "prod":
     def run(updater):
-        PORT = int(os.environ.get("PORT", "8443"))
+        PORT = int(os.environ.get("PORT", "5000"))
         HEROKU_APP_NAME = os.environ.get("therealshow")
         # Code from https://github.com/python-telegram-bot/python-telegram-bot/wiki/Webhooks#heroku
         updater.start_webhook(listen="0.0.0.0",
                               port=PORT,
                               url_path=TOKEN)
-        updater.bot.set_webhook("https://{}.herokuapp.com/{}".format(HEROKU_APP_NAME, TOKEN))
+        updater.bot.set_webhook("https://therealshow.herokuapp.com/" + TOKEN)
+        updater.idle()
 else:
     logger.error("No MODE specified!")
     sys.exit(1)
