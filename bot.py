aa224d4db72b7f72339f283069970e11f628fa1d
Adri <adrica3018@gmail.com>
2019-09-10 17:10:16 +0200

    format update

--- bot.py
+++ bot.py
@@ -241,7 +241,7 @@ def crearEquipos(bot, update):
     if(update.message.from_user.id == 892752079):
         bot.send_message(
             chat_id=update.message.chat_id,
-            text="\t ⚽️ PACHRICOLAJE 🔨 ⚽️'''\n\n ⚪️ Equipo BLANCO: \n ➖ Alfredo\n ➖ Urri \n ➖ Guille\n ➖ Cosmin\n ➖ Joche\n\n 🔵 Equipo COLOR:\n ➖ Joseca\n ➖ Banegas\n ➖ Zamo\n ➖ Fran\n ➖ Pablo",
+            text="\t PACHRICOLAJE 🔨\n 🗓 Martes 10 \n 🕑 19:00\n 🏟 UMU\n ⚪️ Equipo BLANCO: \n ➖ Alfredo\n ➖ Urri \n ➖ Guille\n ➖ Cosmin\n ➖ Joche\n\n 🔴 Equipo COLOR:\n ➖ Joseca\n ➖ Banegas\n ➖ Zamo\n ➖ Fran\n ➖ Pablo",
             parse_mode= ParseMode.MARKDOWN
         )
     else:
