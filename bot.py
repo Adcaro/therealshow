4705af7221a4112e74e7d4ebd520274ebdaeaed1
Adri <adrica3018@gmail.com>
2020-07-03 18:45:49 +0200

    format message change

--- bot.py
+++ bot.py
@@ -296,7 +296,7 @@ def mystats(bot, update, args):
         downloadCard(j[6])
         #Mostrar mensaje
         senderText = "📊 Stats de {} Season 2 📊\n".format(j[0])
-        bot.send_photo(chat_id=update.message.chat_id, photo=open(j[6], 'rb'), caption =senderText + "\n\t🥇 Goles : " + str(j[1]) + "\n\t🥈 Asist: " + str(j[2]) + "\n\t🥉 P. Ganados: " + str(j[3]) + "\n\t 😑Empates: " + str(j[4]) + "\n\t🥺 P. Perdidos: " + str(j[5]-j[3]) + "\n\t⚽ P. Jugados: " + str(j[5]))
+        bot.send_photo(chat_id=update.message.chat_id, photo=open(j[6], 'rb'), caption =senderText + "\n\t🥇 Goles : " + str(j[1]) + "\n\t🥈 Asist: " + str(j[2]) + "\n\t🥉 P. Ganados: " + str(j[3]) + "\n\t😑 Empates: " + str(j[4]) + "\n\t🥺 P. Perdidos: " + str(j[5]-j[3]-j[4]) + "\n\t⚽ P. Jugados: " + str(j[5]))
 
 #Comando para mostrar los partidos asistidos
 #R
